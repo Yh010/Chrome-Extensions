05b28ead67b5601ede150a5f2ec0c6bebe600bcf
Yh010 <yash4success@gmail.com>
2024-08-08 11:47:12 +0530

    twitter extension implemented bois

--- reading-time/scripts/content.js
+++ reading-time/scripts/content.js
@@ -1,22 +1,19 @@
-const article = document.querySelector("article");
+const tweets = document.querySelectorAll('article div[data-testid="tweetText"] span[class="css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-poiln3"]');
 
-// `document.querySelector` may return null if the selector doesn't match anything.
-if (article) {
-  const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
-  const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
-  const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 200);
-  const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `⏱️ ${readingTime} min read`;
+tweets.forEach(tweet => {
+  if (tweet) {
+    const text = tweet.textContent;
+    const wordMatchRegExp = /[^\s]+/g;
+    const words = text.matchAll(wordMatchRegExp);
+    const wordCount = [...words].length;
+    const readingTime = Math.round(wordCount / 200);
+    const badge = document.createElement("p");
+    badge.classList.add("color-secondary-text", "type--caption");
+    badge.textContent = `⏱️ ${readingTime} min read`;
 
-  // Support for API reference docs
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode;
-
-  (date ?? heading).insertAdjacentElement("afterend", badge);
-}
+    const tweetContainer = tweet.closest('article');
+    if (tweetContainer) {
+      tweetContainer.insertAdjacentElement("beforeend", badge);
+    }
+  }
+});
